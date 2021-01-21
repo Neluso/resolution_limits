2dcd24b4a79a25d3ba536fe42a2c5c9b3264bbe7
Neluso <neluso.mu@gmail.com>
2021-01-21 08:26:36 +0100

    2 layer - measured traces

---
+++ plot_fits_real_refs_2layers.py
@@ -0,0 +1,102 @@
+from TDSA import *
+import scipy.interpolate as intplt
+from scipy.optimize import differential_evolution
+
+
+deg_in = 0  # incidence angle in degrees
+snell_sin = n_air * sin(deg_in * pi / 180)
+# n_subs = 1.17 - 0.0 * 1j  # substrate refractive index -- cork
+n_subs = 1e20 - 0.0 * 1j  # substrate refractive index -- metal
+eps = 1e-20
+
+
+# function definitions
+def theta(n):
+    return arcsin(snell_sin / real(n))
+
+
+def ct2(n_l, n_l_1):
+    n_l *= cos(theta(n_l))
+    n_l_1 *= cos(theta(n_l_1))
+    return 4 * n_l * n_l_1 / (n_l + n_l_1) ** 2
+
+
+def cr_l_1_l(n_l, n_l_1):  # from n_l-1 to n_l
+    n_l_1 *= cos(theta(n_l_1))
+    n_l *= cos(theta(n_l))
+    return (n_l_1 - n_l) / (n_l_1 + n_l)
+
+
+def phase_factor(n, k, thick, freq):  # theta in radians
+    omg = 2 * pi * freq
+    thick *= cos(theta(n))
+    phi = 2 * omg * thick / c_0
+    return exp(- 1j * n * phi) * exp(- k * phi)
+
+
+def recover_material(freqs, material):  # ['ABS', 'HDPE', 'PA6', 'PAMD', 'PC', 'PET', 'PFTE', 'PMMA', 'PP']
+    fn_read, n_read = read_1file('./sim_resources/polymer_database/n_' + material + '.csv')
+    fa_read, alpha_read = read_1file('./sim_resources/polymer_database/alpha_' + material + '.csv')
+    n_intplt = intplt.interp1d(fn_read, n_read, bounds_error=False, fill_value=(n_read[0], n_read[-1]))
+    n = n_intplt(freqs)
+    alpha_intplt = intplt.interp1d(fa_read, alpha_read, bounds_error=False, fill_value=(alpha_read[0], alpha_read[-1]))
+    k = 1e-10 * c_0 * alpha_intplt(freqs) / (4 * pi * freqs + eps)
+    return n, k
+
+
+def H_sim(freq, n_i, k_i, thick_i, n_o, k_o, thick_o, d_air):
+    H_i = cr_l_1_l(n_subs, n_i - 1j * k_i)
+
+    rlm1l = cr_l_1_l(n_i - 1j * k_i, n_o - 1j * k_o)
+    tt = ct2(n_i - 1j * k_i, n_o - 1j * k_o)
+    exp_phi = phase_factor(n_i, k_i, thick_i, freq)
+
+    H_i = rlm1l + (tt * H_i * exp_phi) / (1 + rlm1l * H_i * exp_phi)
+
+    rlm1l = cr_l_1_l(n_o - 1j * k_o, n_air_cplx)
+    tt = ct2(n_o - 1j * k_o, n_air_cplx)
+    exp_phi = phase_factor(n_o, k_o, thick_o, freq)
+
+    H_i = rlm1l + (tt * H_i * exp_phi) / (1 + rlm1l * H_i * exp_phi)
+
+    return exp(- 1j * 2 * 2 * pi * freq * d_air / c_0) * H_i
+
+
+def cost_function(params, *args):
+    d_air, thick_i, thick_o = params
+    E_sam, E_ref_w, freqs, n_i, k_i, n_o, k_o = args
+    H_teo = H_sim(freqs, n_i, k_i, thick_i, n_o, k_o, thick_o, d_air)
+    E_teo = irfft(H_teo * E_ref_w)
+    return sum((E_sam - E_teo) ** 2)
+
+
+
+in_dir = './output/simulation_real_refs/2_layer/traces/'
+ref_dir = './sim_resources/refs/'
+data_base_dir = './sim_resources/polymer_database/'
+dir_list = os.listdir(in_dir)
+rows = len(dir_list)
+columns = 5
+data = list()
+fh = open('./output/simulation_real_refs/2_layer/results.txt', 'r')
+i = 0
+for line in fh:
+    line = line.replace('\n', '')
+    line = line.replace('k', '')
+    line = line.split(' ')
+    for j in range(columns):
+        line[j] = float(line[j])
+    data.append(tuple(line))
+
+data = array(data)
+
+sort_idxs = argsort(data, axis=0)[:, 0]
+data = data[sort_idxs]
+for i in range(4):
+    figure(i+1)
+    loglog(sort(data[8 * i:8 * i + 7, 1]), sort(data[8 * i:8 * i + 7, 1]), 'k-', lw=1.0, label='sims')
+    loglog(data[8 * i:8 * i + 7, 1], data[8 * i:8 * i + 7, 2], '.', label='inner')
+    loglog(data[8 * i:8 * i + 7, 1], data[8 * i:8 * i + 7, 3], '.', label='outer')
+    title(data[8 * i, 0])
+    legend()
+show()
---
+++ sim_real_refs_2layers.py
@@ -0,0 +1,93 @@
+from TDSA import *
+import scipy.interpolate as intplt
+
+
+deg_in = 0  # incidence angle in degrees
+snell_sin = n_air * sin(deg_in * pi / 180)
+# n_subs = 1.17 - 0.0 * 1j  # substrate refractive index -- cork
+n_subs = 1e20 - 0.0 * 1j  # substrate refractive index -- metal
+eps = 1e-20
+
+
+# function definitions
+def theta(n):
+    return arcsin(snell_sin / real(n))
+
+
+def ct2(n_l, n_l_1):
+    n_l *= cos(theta(n_l))
+    n_l_1 *= cos(theta(n_l_1))
+    return 4 * n_l * n_l_1 / (n_l + n_l_1) ** 2
+
+
+def cr_l_1_l(n_l, n_l_1):  # from n_l-1 to n_l
+    n_l_1 *= cos(theta(n_l_1))
+    n_l *= cos(theta(n_l))
+    return (n_l_1 - n_l) / (n_l_1 + n_l)
+
+
+def phase_factor(n, k, thick, freq):  # theta in radians
+    omg = 2 * pi * freq
+    thick *= cos(theta(n))
+    phi = 2 * omg * thick / c_0
+    return exp(- 1j * n * phi) * exp(- k * phi)
+
+
+def recover_material(freqs, material):  # ['ABS', 'HDPE', 'PA6', 'PAMD', 'PC', 'PET', 'PFTE', 'PMMA', 'PP']
+    fn_read, n_read = read_1file('./sim_resources/polymer_database/n_' + material + '.csv')
+    fa_read, alpha_read = read_1file('./sim_resources/polymer_database/alpha_' + material + '.csv')
+    n_intplt = intplt.interp1d(fn_read, n_read, bounds_error=False, fill_value=(n_read[0], n_read[-1]))
+    n = n_intplt(freqs)
+    alpha_intplt = intplt.interp1d(fa_read, alpha_read, bounds_error=False, fill_value=(alpha_read[0], alpha_read[-1]))
+    k = 1e-10 * c_0 * alpha_intplt(freqs) / (4 * pi * freqs + eps)
+    return n, k
+
+
+def H_sim(freq, n_i, k_i, thick_i, n_o, k_o, thick_o, d_air):
+    H_i = cr_l_1_l(n_subs, n_i - 1j * k_i)
+
+    rlm1l = cr_l_1_l(n_i - 1j * k_i, n_o - 1j * k_o)
+    tt = ct2(n_i - 1j * k_i, n_o - 1j * k_o)
+    exp_phi = phase_factor(n_i, k_i, thick_i, freq)
+
+    H_i = rlm1l + (tt * H_i * exp_phi) / (1 + rlm1l * H_i * exp_phi)
+
+    rlm1l = cr_l_1_l(n_o - 1j * k_o, n_air_cplx)
+    tt = ct2(n_o - 1j * k_o, n_air_cplx)
+    exp_phi = phase_factor(n_o, k_o, thick_o, freq)
+
+    H_i = rlm1l + (tt * H_i * exp_phi) / (1 + rlm1l * H_i * exp_phi)
+
+    return exp(- 1j * 2 * 2 * pi * freq * d_air / c_0) * H_i
+
+
+def sim_traces(mat_i, mat_o):
+    t0 = time_ns()
+    out_dir = './output/simulation_real_refs/2_layer/traces/'
+    if not os.path.isdir(out_dir):
+        os.mkdir(out_dir)
+    in_dir = './sim_resources/refs/'
+    ref_list = os.listdir(in_dir)
+    for ref_file in ref_list:
+        if ref_file.split('_')[1] == '1.txt':
+            t_ref, E_ref = read_1file(in_dir + ref_file)  # t_ref in ps
+            t_ref *= 1e-12
+            f_ref, E_ref_w = fourier_analysis(t_ref, E_ref)  # f_ref in THz
+            n_i, k_i = recover_material(f_ref, mat_i)
+            n_o, k_o = recover_material(f_ref, mat_o)
+            for d_mat in pow(10, arange(-1, 3, 1 / 2)):
+                name_trace = str(d_mat) + '_' + mat_i + '_' + mat_o + '_' + ref_file.replace('_1', '')
+                d_mat *= 1e-6  # um
+                H_sim_teo = H_sim(f_ref, n_i, k_i, d_mat, n_o, k_o, d_mat, 0.0)
+                E_sim_w = H_sim_teo * E_ref_w
+                E_sim = irfft(E_sim_w)
+                tw = open(out_dir + name_trace, 'w')
+                for i in range(E_sim.size):
+                    tw.write(str(t_ref[i] * 1e12) + ',' + str(E_sim[i]) + '\n')
+                tw.close()
+        else:
+            continue
+    return 0
+
+
+sim_traces('PP', 'PET')  # ['ABS', 'HDPE', 'PA6', 'PAMD', 'PC', 'PET', 'PFTE', 'PMMA', 'PP']
